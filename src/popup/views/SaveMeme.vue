fef58dc3e51bc604833f3f1c201414ceafd2c82c
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2022-12-17 23:33:16 +0300

    feat: support saving to database

--- src/popup/views/SaveMeme.vue
+++ src/popup/views/SaveMeme.vue
@@ -3,12 +3,17 @@ import { ref } from 'vue'
 import { useAsyncState } from '@vueuse/core'
 import { Image } from 'image-js'
 import TesseractService from '~/services/tesseract'
+import { SupabaseClient, saveMeme } from '~/services/supabase'
 
 const queryParams = new URLSearchParams(location.search)
 const imageUrl = decodeURIComponent(queryParams.get('imageUrl') || '')
 const finalImageUrl = ref('')
 
-const { state: imageText, isLoading, error } = useAsyncState(async () => {
+const {
+  state: imageText,
+  isLoading,
+  error,
+} = useAsyncState(async () => {
   const image = await Image.load(imageUrl)
   finalImageUrl.value = image.medianFilter().grey().mask().toDataURL()
   const tesseractService = new TesseractService()
@@ -19,9 +24,20 @@ const { state: imageText, isLoading, error } = useAsyncState(async () => {
   await tesseractService.terminate()
   return text
 }, null)
+
+const onSave = async () => {
+  const {
+    data: { user },
+  } = await SupabaseClient.auth.getUser()
+  // TOOD: add hint
+  saveMeme(user, imageText.value, '', imageUrl)
+}
 </script>
 
 <template>
   <h1>{{ isLoading || imageText }}</h1>
-  <img :src="finalImageUrl" />
+  <img :src="finalImageUrl">
+  <button @click="onSave()">
+    Save
+  </button>
 </template>
